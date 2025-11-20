09d80edcbba2bddc394da542559044b179aa0484
Mohammed Swofvan P <swofvanmhd@gmail.com>
2025-11-20 20:50:09 +0530

    Day 5 classwork

--- Day5classwork/greeting/views.py
+++ Day5classwork/greeting/views.py
@@ -6,33 +6,14 @@ from .forms import Loginform
 
 def greeting(request):
     if request.method == 'POST':
-        form = Loginform(request.POST)
-
-        # if form.is_valid():
-        #     return render (request, 'formdata.html', {
-        #     'Formdata' : request.POST,
-        #     'Email' : form.email
-        #})
+        form = Loginform(request.POST)    
 
         if form.is_valid():
             email = form.cleaned_data['email']
             return render(request, 'formdata.html', {'Email': email})
         return render(request, 'login.html', {'form': form})
+    
     form = Loginform()
-    return render(request, 'login.html', {'form' : form})
-
-
-# def greeting(request):
-#     if request.method == 'POST':
-#         form = Loginform(request.POST)
-
-#         if form.is_valid():   # <-- This is required
-#             email = form.cleaned_data['email']
-#             return render(request, 'formdata.html', {"Email": email})
-
-#         # If not valid → return form with errors
-#         return render(request, 'login.html', {"form": form})
-
-#     # GET request → blank form
-#     form = Loginform()
-#     return render(request, 'login.html', {"form": form})
+    return render(request, 'login.html', {
+        'form' : form
+        })
